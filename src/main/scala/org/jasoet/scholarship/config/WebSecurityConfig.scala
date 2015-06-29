a8da03523129c72f9ad16fd6f25971fcd8eeeee0
Deny Prasetyo <deny.prasetyo@gdplabs.id>
2015-06-30 05:07:25 +0700

    Add Support For RedisSpringSession

--- src/main/scala/org/jasoet/scholarship/config/WebSecurityConfig.scala
+++ src/main/scala/org/jasoet/scholarship/config/WebSecurityConfig.scala
@@ -14,10 +14,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
 
 @Configuration
 @EnableWebMvcSecurity
 @EnableConfigurationProperties
+@EnableRedisHttpSession
 class WebSecurityConfig extends AltairSecuritySupport {
 
   @throws(classOf[Exception])
