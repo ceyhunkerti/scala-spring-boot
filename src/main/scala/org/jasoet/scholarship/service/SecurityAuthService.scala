0714f3dae15b2d349cd4b4714469462edbec6f52
Deny Prasetyo <deny.prasetyo@gdplabs.id>
2015-06-30 16:32:37 +0700

    Modify Prefix and SecurityAuth to accomodate different Login type

--- src/main/scala/org/jasoet/scholarship/service/SecurityAuthService.scala
+++ src/main/scala/org/jasoet/scholarship/service/SecurityAuthService.scala
@@ -1,11 +1,14 @@
 package org.jasoet.scholarship.service
 
+import org.jasoet.scholarship.config.auth.properties.SecurityUtilProperties
 import org.jasoet.scholarship.config.auth.{AltairUser, AltairUserDetails}
 import org.jasoet.scholarship.db.entity.Admin
 import org.jasoet.scholarship.db.repository.AdminRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
+
+import scala.collection.JavaConversions._
 
 /**
  * Created by Deny Prasetyo
@@ -17,12 +20,35 @@ import org.springframework.stereotype.Service
 
 
 @Service
-class SecurityAuthService @Autowired()(adminRepository: AdminRepository) {
+class SecurityAuthService @Autowired()(adminRepository: AdminRepository, securityUtilProperties: SecurityUtilProperties) {
   @throws[UsernameNotFoundException]
   def fetchUser(usernameOrEmail: String): AltairUserDetails[AltairUser] = {
-    Option(adminRepository.findOne(usernameOrEmail)) match {
+    val listPrefixes = securityUtilProperties.getRolePrefix.toList
+    val splitUsername = usernameOrEmail.split("-").toList
+
+    splitUsername match {
+      case prefix :: username :: Nil =>
+        listPrefixes.find(_._2.equalsIgnoreCase(prefix)) match {
+          case Some((k, v)) => prefixRoleHandler(k, username)
+          case None => defaultHandler(username)
+        }
+      case prefix :: Nil =>
+        defaultHandler(usernameOrEmail)
+      case Nil => throw new UsernameNotFoundException(s"Username/Email $usernameOrEmail not Found!")
+    }
+  }
+
+  private def prefixRoleHandler(role: String, username: String): AltairUserDetails[AltairUser] = {
+    role.toUpperCase match {
+      case "SUPER_ADMIN" => defaultHandler(username)
+      case _ => defaultHandler(username)
+    }
+  }
+
+  private def defaultHandler(username: String): AltairUserDetails[AltairUser] = {
+    Option(adminRepository.findOne(username)) match {
       case Some(a@Admin(email, name, roles, enabled)) => AltairUserDetails(a, roles)
-      case None => throw new UsernameNotFoundException(s"Username/Email $usernameOrEmail not Found!")
+      case None => throw new UsernameNotFoundException(s"Username/Email $username not Found!")
     }
   }
 }
